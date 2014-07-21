752f8d9db72236b8b85197bf09979b3f2474f3f0
Willy Douglas <willygdouglas@gmail.com>
2014-07-20 20:31:36 -0600

    Add second board for 'next' state.

--- js/gameOfLife.js
+++ js/gameOfLife.js
@@ -29,34 +29,62 @@ var WORLD = WORLD || {};
         this.getWidth = function () { return _width; };
         this.getHeight = function () { return _height; };
 
-        this._board = [];
-        initializeBoard(this._board, _height);
+        // Define two boards:
+        // A current board that represents the working state,
+        // and a next Board that represents the in progress state
+        this._boards = [[],[]];
+
+        this.currentBoard = this._boards[0];
+        this.nextBoard = this._boards[1];
+        this._boardState = 0;
+
+        initializeBoard(this._boards[0], _height);
+        initializeBoard(this._boards[1], _height);
 
         // Board will still need to be seeded at this point
+    };
+
+    // Swap board state references
+    // After calling this function, any external references to
+    // the .currentBoard or .nextBoard of a game will need to be updated
+    WORLD.Game.prototype.updateBoard = function () {
+        if (this._boardState === 0) {
+            this.currentBoard = this._boards[1];
+            this.nextBoard = this._boards[0];
+            this._boardState = 1;
+        }
+        else {
+            this.currentBoard = this._boards[0];
+            this.nextBoard = this._boards[1];
+            this._boardState = 0;
+        }
     };
 
     WORLD.Game.prototype.mapWholeBoard = function (fnEach, done, thisArg) {
         var i, j, w, h, val, returnedVal;
         thisArg = thisArg || this;
-        for (i = 0, w = this.getWidth(); i < w; ++i) {
-            for (j = 0, h = this.getHeight(); j < h; ++j) {
-                val = this._board[i][j];
+
+        for (i = 0, h = this.getHeight(); i < h; ++i) {
+            for (j = 0, w = this.getWidth(); j < w; ++j) {
+                val = this.currentBoard[i][j];
                 returnedVal = fnEach.call(thisArg, val, i, j);
 
                 // We assume that if the user passes a value back that is not
                 // false, it will an acceptable value.
                 // This is a sacrifice of some robustness for better performance.
                 if (returnedVal !== undefined && returnedVal !== false) {
-                    this._board[i][j] = returnedVal;
+                    this.nextBoard[i][j] = returnedVal;
                 }
             }
         }
+        this.updateBoard();
+
         if (done) {
             done.call(thisArg);
         }
     };
 
-    // Makes board entirely zero
+    // Makes previous board entirely zero
     // Not particularly interesting, but good for testing
     WORLD.Game.prototype.seedZero = function () {
         this.mapWholeBoard(function () {
@@ -72,12 +100,14 @@ var WORLD = WORLD || {};
 
     WORLD.Game.prototype.HTMLify = function () {
         var i, j, w, h, HTMLstring = '';
-        for (i = 0, w = this.getWidth(); i < w; ++i) {
-            for (j = 0, h = this.getHeight(); j < h; ++j) {
-                HTMLstring += this._board[i][j] + ' ';
+
+        for (i = 0, h = this.getHeight(); i < h; ++i) {
+            for (j = 0, w = this.getWidth(); j < w; ++j) {
+                HTMLstring += this.currentBoard[i][j] + ' ';
             }
             HTMLstring += '<br>';
         }
         return HTMLstring;
     };
+
 })();
--- test/spec/gameOfLife.js
+++ test/spec/gameOfLife.js
@@ -17,9 +17,23 @@ describe('Game', function () {
 
     it('should create a square board if passed an invalid height', function () {
       expect(new WORLD.Game(3).getHeight()).to.equal(3);
+      expect(new WORLD.Game(3, 'hi').getHeight()).to.equal(3);
     });
 
-    it('should make any point on the board accessible through a pair of indices',
+    it('should define an array of two boards (current and next)', function () {
+      var game = new WORLD.Game(3);
+
+      expect(game._boards.length).to.equal(2);
+    });
+
+    it('should provide access to current and next board', function () {
+      var game = new WORLD.Game(2);
+
+      expect(game.currentBoard).to.be.an('array');
+      expect(game.nextBoard).to.be.an('array');
+    });
+
+    it('should make any point on the boards accessible through a pair of indices',
           function () {
 
       var game = new WORLD.Game(5);
@@ -27,22 +41,56 @@ describe('Game', function () {
       // that the inner index can access a point in that array
       // without throwing an error.
       // Reminder: Zero based array index (4 is largest in board size 5).
-      expect(game._board[4]).to.be.an('array');
+      expect(game.currentBoard[4]).to.be.an('array');
+      expect(game.nextBoard[4]).to.be.an('array');
+    });
+  });
+
+  describe('.updateBoard', function () {
+    it('should swap the context of current and next boards', function () {
+      var game = new WORLD.Game(2);
+
+      game.currentBoard[0][0] = 0;
+      game.nextBoard[0][0] = 1;
+
+      game.updateBoard();
+      expect(game.currentBoard[0][0]).to.equal(1);
+      expect(game.nextBoard[0][0]).to.equal(0);
+
+      game.updateBoard();
+      expect(game.currentBoard[0][0]).to.equal(0);
+      expect(game.nextBoard[0][0]).to.equal(1);
+    });
+
+    it('should change the board state', function () {
+      var game = new WORLD.Game(0),
+          initialBoardState = game._boardState;
+
+      game.updateBoard();
+      expect(game._boardState).to.not.equal(initialBoardState);
     });
   });
 
   describe('.mapWholeBoard', function () {
-    var game, boardSize = 2;
+    var game,
+        boardWidth = 2,
+        boardHeight = 3;
     beforeEach(function () {
-      game = new WORLD.Game(boardSize);
+      game = new WORLD.Game(boardWidth, boardHeight);
     });
 
-    it('should iterate through each point in the board', function (done) {
+    it('should iterate through each point in the current board', function (done) {
       var count = 0;
-      game.mapWholeBoard(function () {
+
+      game.currentBoard[0][1] = 1;
+
+      game.mapWholeBoard(function (val, i, j) {
         count += 1;
+        if (i === 0 && j === 1) {
+          expect(val).to.equal(1);
+        }
       }, function () {
-        expect(count).to.equal(boardSize*boardSize);
+        expect(count).to.equal(boardWidth*boardHeight);
         done();
       });
     });
@@ -51,10 +99,11 @@ describe('Game', function () {
       game.mapWholeBoard(function () {
         return 0;
       }, function () {
-        expect(game._board[0][0]).to.equal(0);
-        expect(game._board[0][1]).to.equal(0);
-        expect(game._board[1][0]).to.equal(0);
-        expect(game._board[boardSize-1][boardSize-1]).to.equal(0);
+        var currBoard = game.currentBoard;
+        expect(currBoard[0][0]).to.equal(0);
+        expect(currBoard[0][1]).to.equal(0);
+        expect(currBoard[1][0]).to.equal(0);
+        expect(currBoard[boardHeight-1][boardWidth-1]).to.equal(0);
         done();
       });
     });
@@ -72,32 +121,35 @@ describe('Game', function () {
           doneReturnValue = this.testContext();
         }, this);
 
-        expect(game._board[0][0]).to.equal(1);
+        expect(game.currentBoard[0][0]).to.equal(1);
         expect(doneReturnValue).to.equal(1);
     });
   });
 
   describe('.seedZero', function () {
-    it('should set all the board values to 0', function () {
-      var game = new WORLD.Game(2);
+    it('should set all the current board values to 0', function () {
+      var game = new WORLD.Game(2),
+          currBoard;
 
       game.seedZero();
-      expect(game._board[0][0]).to.equal(0);
-      expect(game._board[0][1]).to.equal(0);
-      expect(game._board[1][0]).to.equal(0);
-      expect(game._board[1][1]).to.equal(0);
+
+      currBoard = game.currentBoard
+      expect(currBoard[0][0]).to.equal(0);
+      expect(currBoard[0][1]).to.equal(0);
+      expect(currBoard[1][0]).to.equal(0);
+      expect(currBoard[1][1]).to.equal(0);
     });
   });
 
   describe('.HTMLify', function () {
     it('should return a string of values with \'<br>\'s', function () {
-      var boardSize = 2,
-          game = new WORLD.Game(boardSize);
+      var boardWidth = 2,
+          game = new WORLD.Game(boardWidth);
       game.seedZero();
 
       var html = game.HTMLify();
       expect(html).to.be.a('string');
-      expect(html.match(/(<br>)/g).length).to.equal(boardSize);
+      expect(html.match(/(<br>)/g).length).to.equal(boardWidth);
     });
   });
 
