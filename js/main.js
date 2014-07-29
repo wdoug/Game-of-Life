2ece7d9df34e255c03096471f4148e4bac2d4ba2
Willy Douglas <willygdouglas@gmail.com>
2014-07-29 00:43:25 -0600

    Add basic form for creating a new game of life board.

--- js/main.js
+++ js/main.js
@@ -18,4 +18,30 @@ var WORLD = WORLD || {};
         game.tick();
         $display.html( game.stringify() );
     }, 50);
+
+
+    // User input form for new board
+    $('#new-board-btn').on('click', function (event) {
+        event.preventDefault();
+
+        var height = parseInt( $('#new-height').val() ),
+            width = parseInt( $('#new-width').val() );
+
+        if (isNaN(height) || isNaN(width)) {
+            // @TODO: replace alert with custom element
+            alert('Please input valid height and width values');
+            return;
+        }
+        else {
+            try {
+                game = new WORLD.Game(height, width);
+                game.seedRandom();
+            } catch (e) {
+                // @TODO: Handle errors, and provide
+                // feedback without an alert
+                alert('there was an error');
+            }
+        }
+
+    });
 })();
