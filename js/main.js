20aef61325a15bd53e53ef822abf54d4aeee5ad5
Willy Douglas <willygdouglas@gmail.com>
2014-07-29 12:16:13 -0600

    Use innerHTML instead of jQuery for updating board display.

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@ var WORLD = WORLD || {};
 
 (function () {
     var game = new WORLD.Game(50);
-    var $display = $('#text-board-display');
+    var display = document.getElementById('text-board-display');
 
     game.seedRandom();
 
     // @TODO: Look into swapping this with the requestAnimationFrame api
     setInterval(function () {
         game.tick();
-        $display.html( game.stringify() );
+        display.innerHTML = game.stringify();
     }, 50);
 
 
